1a13a73f305b99846f6fb8c076986ae2be021e49
johnziegenbein <johnziegenbein1993@gmail.com>
2020-08-12 18:44:19 +0200

    Add tavern tier to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import {MatInputModule} from "@angular/material/input";
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
